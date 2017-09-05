b5d52b553b0b3c3b5ab3769ff1f77b7b8cef4fa3
Andrea Matteini <a.matteini@mes-info.de>
2017-09-05 20:40:38 +0200

    Improved style and JS

--- js/playlists.js
+++ js/playlists.js
@@ -1,65 +1,64 @@
 var events = [
-  '170831'
+  '170831',
+  '170907'
 ];
 
-var param = getUrlParameter().toString();
+var eventId = getUrlParameter().toString();
 var items = [];
 
-if ( param != '' && jQuery.inArray( param, events ) > -1 ) {
+if ( eventId != '' && jQuery.inArray( eventId, events ) > -1 ) {
   // Show single event
-  console.log('Show event '+param);
+  log('Show event '+eventId);
 
-  var eventId = param;
   var requestURL = 'json/playlists/'+ eventId +'.json';
   //var requestURL = 'https://raw.githubusercontent.com/andreabaum/andreabaum/master/'+ eventId +'.json';
 
+  setTitle( eventId );
+
   $.getJSON( requestURL, function( data ) {
-    populatePlaylist( data );
+    // Populate playlist
+    $.each( data, function( index, item ) {
+      genre = item.Genre == '-' ? 'cortina' : item.Genre.toLowerCase();
+      year = item.Year == null ? '' :  item.Year
+      items.push( "<li id='" + index + "' class='list-group-item list-group-item-action "+ genre +"' >"+ item.Artist +" - "+ item.Title +"<span class='year'> "+ year +"</span></li>" );
+    });
+
+    setContent();
   });
+
 } else {
   // Show events list
-  console.log('Show events list');
+  log('Show events list');
+
+  setTitle( 'Events' );
 
   $.each( events, function( index, item ) {
-    items.push( "<a href='?"+ item +"' class='list-group-item list-group-item-action' >" + item + "</a>" );
+    items.push( "<a href='?"+ item +"' class='list-group-item' >" + item + "</a>" );
   });
 
+  setContent();
+}
+
+function setTitle( title ) {
   $( "<span/>", {
     "class": "",
-    html: 'Events'
+    html: title
   }).appendTo( ".title" );
+}
 
+function setContent() {
   $( "<ul/>", {
     "class": "list-group borderless",
     html: items.join( "" )
   }).appendTo( ".content" );
 }
 
-
-
-function populatePlaylist( data ) {
-  var playlist = data['items'];
-
-  $.each( data, function( index, item ) {
-    genre = item.Genre == '-' ? 'cortina' : item.Genre.toLowerCase();
-    year = item.Year == null ? '' :  item.Year
-    items.push( "<li id='" + index + "' class='list-group-item " + genre + "' >" + item.Artist + ' - ' + item.Title + "<span class='year'> " + year + "</span></li>" );
-  });
-
-  $( "<span/>", {
-    "class": "",
-    html: eventId
-  }).appendTo( ".title" );
-
-  $( "<ul/>", {
-    "class": "list-group borderless",
-    html: items.join( "" )
-  }).appendTo( ".playlist" );
+function getUrlParameter() {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+      sURLVariables = sPageURL.split('&');
+  return sURLVariables[0];
 }
 
-
-function getUrlParameter() {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-        sURLVariables = sPageURL.split('&');
-    return sURLVariables[0];
-};
+function log ( obj ) {
+  console.log(obj);
+}
